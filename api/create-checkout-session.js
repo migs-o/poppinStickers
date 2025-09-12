c5f37f82210ecf201efbd088b338492047003202
migs-o <mikeortiz@gmail.com>
2025-09-12 17:11:22 -0600

    morefix

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
       success_url: "https://poppin-stickers.vercel.app/success",
       cancel_url: "https://poppin-stickers.vercel.app/cancel",
     });
+    
 
     res.status(200).json({ url: session.url });
   } catch (err) {
